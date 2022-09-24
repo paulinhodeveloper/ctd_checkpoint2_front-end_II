bdde48af60eed7d7e194928d4802eadd391f82c8
braulioportela79 <braulioportela@outlook.com>
2022-09-24 15:54:16 -0300

    Botao Editar Descrição Tarefa / Refatorando

---
+++ assets/scripts/modules/checkUserTask.js
@@ -0,0 +1,41 @@
+import { getTask } from '../api/getTask.js';
+import { updateTask } from '../api/updateTask.js';
+import { renderUserTasks } from './renderUserTasks.js';
+
+export const checkUserTask = (e, token) => {
+
+    // Selectionar a tarefa
+    let task = e.parentElement.parentElement;
+    let id = task.getAttribute('task-id');
+    getTask(token, id);
+
+    // Setar tempo para pegar a tarefa
+    setTimeout(() => {
+
+        // Pegar a tarefa no storage
+        const userTask = sessionStorage.getItem('selectTask');
+        const userTaskObj = JSON.parse(userTask);
+
+        // Definir a tarefa como concluída/não concluída
+        userTaskObj.completed ? userTaskObj.completed = false : userTaskObj.completed = true;
+
+        // Atualizar a tarefa na API
+        let userTaskJson = JSON.stringify(userTaskObj);
+        sessionStorage.setItem('selectTask', userTaskJson);
+        updateTask(token, id, userTaskJson);
+
+        // Atualizar a lista de tarefas no storage
+        let updatedTask = userTaskObj;
+        const userTasks = sessionStorage.getItem('userTasks');
+        const userTasksObj = JSON.parse(userTasks);
+        const indexOfItemInArray = userTasksObj.findIndex(q => q.id == updatedTask.id);
+        if (indexOfItemInArray > -1) {
+            userTasksObj[indexOfItemInArray] = updatedTask;
+        };
+        let userTasksJson = JSON.stringify(userTasksObj);
+        sessionStorage.setItem('userTasks', userTasksJson);
+
+        // Reinderizar as tarefas
+        renderUserTasks(token);
+    }, 500);
+}
---
+++ assets/scripts/modules/editUserTask.js
@@ -0,0 +1,117 @@
+import { getTask } from '../api/getTask.js';
+import { updateTask } from '../api/updateTask.js';
+
+export const editUserTask = (e, token, event) => {
+    // Variáveis dos Elementos da Tarefa
+    let task = e.parentElement.parentElement;
+    let id = task.getAttribute('task-id');
+    let taskDesc = e.parentElement.parentElement.childNodes[3];
+    let taskHeader = e.parentElement;
+    let cancelUpdateBtn = e.parentElement.parentElement.childNodes[5].children[0].childNodes[1];
+    let saveUpdateBtn = e.parentElement.parentElement.childNodes[5].children[0].childNodes[3];
+
+    // Pegar a tarefa no storage
+    if (e.contains(event.target)) {
+        getTask(token, id);
+    };
+
+    // Setar tempo para pegar a tarefa
+    setTimeout(() => {
+
+        // Pegar a tarefa no storage
+        const userTask = sessionStorage.getItem('selectTask');
+        const userTaskObj = JSON.parse(userTask);
+
+        // Evento de Clique para editar a tarefa (ao clicar no botão / ao clicar na descrição da tarefa)
+        if (e.contains(event.target) || (event.target.id == 'descrip' && document.activeElement == taskDesc)) {
+
+            // Habilitar editar texto na própria descrição da tarefa
+            taskDesc.setAttribute('contentEditable', true);
+
+            // Focar o prompt na descrição da tarefa para editar
+            taskDesc.focus();
+
+            // Ocultar Botões do Header da tarefa
+            taskHeader.style.display = 'none';
+
+            // Aumentar a altura da descrição da tarefa
+            taskDesc.style.height = '174px';
+
+            // Mostrar botões para salvar/cancelar edição da tarefa
+            cancelUpdateBtn.style.display = 'block';
+            saveUpdateBtn.style.display = 'block';
+
+            // Variáveis para texto da descrição atual na tarefa / descrição da tarefa selecionada no storage
+            let currentTextDesc = taskDesc.innerHTML;
+            let currentStorageDesc = userTaskObj.description;
+
+            // Salvar descrição atual da tarefa no storage
+            sessionStorage.setItem('currentTextDesc', currentTextDesc);
+
+            // Evento de key para salvar a descrição da tarefa ao digitar no storage
+            taskDesc.addEventListener('keyup', () => {
+                currentTextDesc = taskDesc.innerHTML
+                sessionStorage.setItem('currentTextDesc', currentTextDesc);
+            });
+
+            // Evento de clique no botão para salvar alteração na descrição da tarefa
+            saveUpdateBtn.addEventListener('click', () => {
+
+                // Pegar a descrição da tarefa salvo no storage
+                let textDescStorage = sessionStorage.getItem('currentTextDesc');
+
+                // Se a descrição da tarefa salva no storage for diferente da descrição da tarefa selecionada no storage
+                if (currentStorageDesc != textDescStorage) {
+
+                    // Atualizar a descrição da tarefa selecionada no storage
+                    userTaskObj.description = textDescStorage;
+
+                    // Atualizar a tarefa na API
+                    let userTaskJson = JSON.stringify(userTaskObj);
+                    sessionStorage.setItem('selectTask', userTaskJson);
+                    updateTask(token, id, userTaskJson);
+
+                    // Atualizar a lista de tarefas no storage
+                    let updatedTask = userTaskObj;
+                    const userTasks = sessionStorage.getItem('userTasks');
+                    const userTasksObj = JSON.parse(userTasks);
+                    const indexOfItemInArray = userTasksObj.findIndex(q => q.id == updatedTask.id);
+                    if (indexOfItemInArray > -1) {
+                        userTasksObj[indexOfItemInArray] = updatedTask;
+                    };
+                    let userTasksJson = JSON.stringify(userTasksObj);
+                    sessionStorage.setItem('userTasks', userTasksJson);
+                };
+            });
+        }
+
+        // Se o clique não for no botão para salvar alteração ou na descrição da tarefa editada
+        else {
+
+            // Desabilitar edição da tarefa
+            taskDesc.setAttribute('contentEditable', false);
+
+            // Esconder botões para salvar/cancelar edição da tarefa
+            saveUpdateBtn.style.display = 'none'
+            cancelUpdateBtn.style.display = 'none'
+
+            // Mostrar Header da tarefa
+            taskHeader.style.display = '';
+
+            // Voltar altura da descrição da tarefa original
+            taskDesc.style.height = '150px';
+
+            // Atualizar a lista de tarefas no storage
+            const userTask = sessionStorage.getItem('selectTask');
+            const userTaskObj = JSON.parse(userTask);
+
+            // Se a tarefa sendo editada tiver alteração do texto, mas não for clicado no botão para salvar
+            if (e.parentElement.parentElement.getAttribute('task-id') == userTaskObj.id) {
+
+                // Descrição da tarefa igual quando foi selecionada(sem alteração)
+                e.parentElement.parentElement.childNodes[3].innerHTML = userTaskObj.description
+            };
+        };
+    }, 500);
+
+}
